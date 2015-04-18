0ee63805d4d2fdac68b633dbbddacc8e92fef606
forana <alex@catalyze.io>
2015-04-18 13:29:02 -0500

    IT'S WORKING

---
+++ bugs/image_routes.py
@@ -0,0 +1,75 @@
+import bottle
+from bottle import request, response
+from PIL import Image, ImageDraw, ImageFont
+import os, tempfile
+import images2gif
+from io import BytesIO
+from bases import mapping as bases
+
+@bottle.route("/image/<base>/<subject>", method = "GET")
+def get_image(base, subject):
+    response.content_type = "image/gif"
+    return bottle.static_file("bugs.gif", root = "bases/")
+
+@bottle.route("/image", method = "POST")
+def new_image():
+    buffer = BytesIO()
+    request.files.get("file").save(buffer)
+    upload = Image.open(buffer).convert(mode = "RGBA")
+
+    base = bases[request.forms.get("base")]
+
+    print base
+
+    im = Image.open("bases/" + base["file"])
+    frames = split_gif(im)
+
+    font = ImageFont.load_default()
+    for i in range(len(frames)):
+        frame = frames[i]
+        direction = base["directions"][i]
+        if direction is not None:
+            size = direction[0]
+            corner = direction[1]
+            rotation = direction[2]
+            scaled_size = choose_scale_rect(size, upload.size)
+            print size, scaled_size, upload.size
+            transformed = upload.resize(scaled_size).rotate(rotation, Image.BILINEAR, True)
+            frame.paste(transformed, corner, transformed)
+
+    tf = tempfile.NamedTemporaryFile(delete = False)
+    filename = tf.name
+    tf.close()
+
+    images2gif.writeGif(filename, frames, duration = base["delay"], subRectangles = False)
+
+    fp = open(filename, "rb")
+    contents = fp.read()
+    fp.close()
+    os.remove(filename)
+
+    response.content_type = "image/gif"
+    return contents
+
+def choose_scale_rect(target_size, image_size):
+    r1 = 1.0*target_size[0]/target_size[1]
+    r2 = 1.0*image_size[0]/image_size[1]
+    if r1 < r2: # scale to width
+        return (target_size[0], int(target_size[0] / r2))
+    else: # scale to height
+        return (int(target_size[1] * r2), target_size[1])
+
+def split_gif(im):
+    frames = []
+    palette = im.getpalette()
+    try:
+        while True:
+            frame = Image.new("RGBA", im.size)
+            if not im.getpalette():
+                im.setpalette(palette)
+            frame.paste(im, (0, 0), im.convert("RGBA"))
+            frames.append(frame)
+            im.seek(im.tell() + 1)
+    except EOFError:
+        pass
+    return frames
--- bugs/server.py
+++ bugs/server.py
@@ -1,1 +1,14 @@
-print "Hello"
+import os
+import bottle
+import image_routes
+
+@bottle.route("/static/<filepath:path>")
+def handle_static(filepath):
+    return bottle.static_file(filepath, root = "static/")
+
+@bottle.route("/")
+def handle_root():
+    return bottle.static_file("index.html", root = "static/")
+
+if __name__ == "__main__":
+    bottle.run(port = os.environ.get("PORT", 8080), host = "0.0.0.0")
